08af9e7c3cee1735b5b7c5460b1c21e21fa329ea
sintornillos <rubiancocarlos@gmail.com>
2022-03-23 12:48:09 -0300

    Document file, env file, and constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,11 @@
-DB: {
-		HOST: 'DB_HOST',
-		PORT: 'DB_PORT',
-		DATABASE: 'DB_DATABASE',
-		USER: 'DB_USER',
-		PASS: 'DB_PASSWORD',
-	  }
+export const Constants = {
+  SERVER_PORT: 'SERVER_PORT',
+  API: '/api',
+  DB: {
+    HOST: 'DB_HOST',
+    PORT: 'DB_PORT',
+    DATABASE: 'DB_DATABASE',
+    USER: 'DB_USER',
+    PASS: 'DB_PASSWORD',
+  },
+};
