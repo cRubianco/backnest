30d481afd4b83a0951f5ac456f3b9f84ec342f3c
sintornillos <rubiancocarlos@gmail.com>
2022-03-29 06:41:39 -0300

    Person entity, Repository, service

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { PersonEntity } from 'src/model/entity/person.entity';
+import { BaseController } from 'src/modules/base/base.controller';
+import { PersonService } from './person.service';
 
-@Controller('person')
-export class PersonController {}
+@Controller('persons')
+export class PersonController extends BaseController<PersonEntity> {
+  
+  constructor (
+    private readonly personService: PersonService 
+  ) {
+    super(personService)
+  }
+
+}
--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PersonEntity } from 'src/model/entity/person.entity';
+import { BaseService } from 'src/modules/base/base.service';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PersonService {}
+export class PersonService extends BaseService<PersonEntity> {
+  
+  constructor(
+    @InjectRepository(PersonEntity)
+    private readonly personsRepository: Repository<PersonEntity>
+  ) {
+    super(personsRepository);
+  }
+  
+}
